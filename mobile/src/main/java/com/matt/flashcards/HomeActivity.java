28882a24f083abcc53af699a7fd21bac31bf42b7
Brandon Selzer <bselzer1@hotmail.com>
2018-03-01 19:09:52 -0500

    Created layouts for screens 2b and 3b.

--- mobile/src/main/java/com/matt/flashcards/HomeActivity.java
+++ mobile/src/main/java/com/matt/flashcards/HomeActivity.java
@@ -3,7 +3,6 @@ package com.matt.flashcards;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -11,7 +10,6 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 
 public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener
@@ -25,7 +23,7 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab_category);
+        FloatingActionButton fab = findViewById(R.id.fab_home);
         fab.setOnClickListener(new View.OnClickListener()
         {
             @Override
