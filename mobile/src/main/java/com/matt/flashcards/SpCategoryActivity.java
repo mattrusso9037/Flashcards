58305b9d9cbc13bd3901e022b05ead4131048d3f
David Siopa <siopa97@hotmail.com>
2018-03-02 20:59:12 -0500

    added classes for single player views and the about view

---
+++ mobile/src/main/java/com/matt/flashcards/SpCategoryActivity.java
@@ -0,0 +1,17 @@
+package com.matt.flashcards;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by David on 3/2/2018.
+ */
+
+public class SpCategoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sp_category);
+    }
+}
