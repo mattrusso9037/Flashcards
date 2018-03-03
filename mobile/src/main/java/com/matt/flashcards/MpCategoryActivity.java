52c1662bfecb99a5ce26d63b8b15168393272037
David Siopa <siopa97@hotmail.com>
2018-03-02 21:40:24 -0500

    testing commit and added the activities to the manifest

---
+++ mobile/src/main/java/com/matt/flashcards/MpCategoryActivity.java
@@ -0,0 +1,17 @@
+package com.matt.flashcards;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by David on 3/2/2018.
+ */
+
+public class MpCategoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mp_category);
+    }
+}
