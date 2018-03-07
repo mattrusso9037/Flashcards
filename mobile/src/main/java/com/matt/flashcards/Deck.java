6c93fbdafda0b8125b550b9404a615efd16dd533
David Pirraglia <dpirraglia2@student.sjcny.edu>
2018-03-07 00:40:09 -0500

    Made slight change to the Deck class

--- mobile/src/main/java/com/matt/flashcards/Deck.java
+++ mobile/src/main/java/com/matt/flashcards/Deck.java
@@ -2,12 +2,11 @@ package com.matt.flashcards;
 
 import java.util.ArrayList;
 
-public class Deck extends ArrayList {
+public class Deck extends ArrayList<Flashcard> {
 
     private String title;
 
     public Deck(String title) {
-
         this.title = title;
     }
 }
