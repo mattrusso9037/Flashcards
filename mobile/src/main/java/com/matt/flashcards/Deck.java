661c57e79905065c4762d7f00a4d43cd1dbdcd9c
David Siopa <siopa97@hotmail.com>
2018-03-07 15:18:32 -0500

    added getter and setter for title

--- mobile/src/main/java/com/matt/flashcards/Deck.java
+++ mobile/src/main/java/com/matt/flashcards/Deck.java
@@ -10,4 +10,13 @@ public class Deck extends ArrayList {
 
         this.title = title;
     }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
 }
