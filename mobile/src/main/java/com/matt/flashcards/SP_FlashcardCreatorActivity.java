e984a62e68e02f7f8b190e1dc4f965f998d997b4
David Pirraglia <dpirraglia2@student.sjcny.edu>
2018-03-07 01:31:57 -0500

    Added sample data to the ListView in the Flashcard Creator Activity

--- mobile/src/main/java/com/matt/flashcards/SP_FlashcardCreatorActivity.java
+++ mobile/src/main/java/com/matt/flashcards/SP_FlashcardCreatorActivity.java
@@ -2,6 +2,7 @@ package com.matt.flashcards;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
 
 public class SP_FlashcardCreatorActivity extends AppCompatActivity {
 
@@ -9,5 +10,13 @@ public class SP_FlashcardCreatorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sp_flashcard_creator);
+
+        // Dummy data
+        Deck dummyFlashcards = new Deck("Dummy Data");
+        dummyFlashcards.add(new Flashcard("MVP", "Minimum Viable Product"));
+        dummyFlashcards.add(new Flashcard("Lorem Ipsum", "dolor sit amet"));
+        dummyFlashcards.add(new Flashcard("Herp", "Derp"));
+
+        ((ListView) findViewById(R.id.flashcards_listview)).setAdapter(new FlashcardAdapter(this, dummyFlashcards));
     }
 }
