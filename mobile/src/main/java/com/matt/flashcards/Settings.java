54a8659608302474957212970502a2428f8f0f6c
David Pirraglia <dpirraglia2@student.sjcny.edu>
2018-03-08 03:20:50 -0500

    Created a Settings class

---
+++ mobile/src/main/java/com/matt/flashcards/Settings.java
@@ -0,0 +1,18 @@
+package com.matt.flashcards;
+
+import java.util.ArrayList;
+
+public final class Settings {
+
+    private Settings() {} // Prevent instantiation of this class
+
+    public final static ArrayList<Deck> theDeckOfDecks = new ArrayList<>();
+
+    public static void loadData() {
+        //
+    }
+
+    public static void saveData() {
+        //
+    }
+}
