7ffd96989e001ebe009bd595ed086a5d540a9ac4
David Pirraglia <dpirraglia2@student.sjcny.edu>
2018-03-08 16:23:40 -0500

    When a Deck is Long Clicked, a Dialog will ask if it should rename or delete said Deck
    - Actions for the rename and delete events have not been added yet

--- mobile/src/main/java/com/matt/flashcards/DeckAdapter.java
+++ mobile/src/main/java/com/matt/flashcards/DeckAdapter.java
@@ -13,8 +13,11 @@ import java.util.ArrayList;
 
 public class DeckAdapter extends ArrayAdapter {
 
+    private ArrayList<Deck> decks;
+
     public DeckAdapter(Context context, ArrayList<Deck> decks) {
         super(context, 0, decks);
+        this.decks = decks;
     }
 
     public View getView(final int position, View convertView, ViewGroup parent) {
@@ -35,6 +38,28 @@ public class DeckAdapter extends ArrayAdapter {
                 Intent flashCardView = new Intent(getContext(), SP_FlashcardViewerActivity.class);
                 flashCardView.putExtra("Index", position);
                 getContext().startActivity(flashCardView);
+            }
+        });
+
+        // Event for when a deck is long clicked
+        listItemView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                new AlertDialog.Builder(getContext())
+                    .setTitle(decks.get(position).getTitle())
+                    .setItems(new String[]{"Rename", "Delete"}, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            switch (which) {
+                                case 0:
+                                    Toast.makeText(getContext(), "Rename", Toast.LENGTH_SHORT).show();
+                                    break;
+                                case 1:
+                                    Toast.makeText(getContext(), "Delete", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }).create().show();
+                return true;
             }
         });
 
