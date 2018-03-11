a1bddab621b38653f93e0c25462dca256abbe571
David Pirraglia <dpirraglia2@student.sjcny.edu>
2018-03-11 03:51:08 -0400

    Created a new activity for creating new and editing flashcards

---
+++ mobile/src/main/java/com/matt/flashcards/AddEditActivity.java
@@ -0,0 +1,13 @@
+package com.matt.flashcards;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class AddEditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_edit);
+    }
+}
