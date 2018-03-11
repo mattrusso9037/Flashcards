3989065c007592a70a3e810a76c81666a2457c15
David Pirraglia <dpirraglia2@student.sjcny.edu>
2018-03-11 12:21:10 -0400

    Fixed bug where blank flashcards can be added to a deck

--- mobile/src/main/java/com/matt/flashcards/AddEditActivity.java
+++ mobile/src/main/java/com/matt/flashcards/AddEditActivity.java
@@ -51,19 +51,28 @@ public class AddEditActivity extends AppCompatActivity {
     // Event for the save button
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        String a = sideA.getText().toString(), b = sideB.getText().toString();
         switch (item.getItemId()) {
             case R.id.action_save:
-                if (editMode) {
-                    currentCard.setSideA(sideA.getText().toString());
-                    currentCard.setSideB(sideB.getText().toString());
+                if (a.isEmpty() && b.isEmpty()){
+                    new AlertDialog.Builder(this)
+                            .setTitle("Error")
+                            .setMessage("You can't save a blank flashcard")
+                            .setPositiveButton("Ok", null)
+                            .create().show();
                 } else {
-                    currentDeck.add(new Flashcard(
-                            sideA.getText().toString(),
-                            sideB.getText().toString()
-                    ));
+                    if (editMode) {
+                        currentCard.setSideA(a);
+                        currentCard.setSideB(b);
+                    } else {
+                        currentDeck.add(new Flashcard(
+                                sideA.getText().toString(),
+                                sideB.getText().toString()
+                        ));
+                    }
+                    Settings.saveData(this);
+                    super.onBackPressed();
                 }
-                Settings.saveData(this);
-                super.onBackPressed();
                 return true;
             // Event for the Up button
             case android.R.id.home: // https://stackoverflow.com/a/8887556
