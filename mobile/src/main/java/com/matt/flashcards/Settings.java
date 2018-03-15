04cbaadedfe71221251e5ca1415368049d6e8f0e
David Pirraglia <dpirraglia2@student.sjcny.edu>
2018-03-15 10:43:05 -0400

    Added comments to the load and save methods

--- mobile/src/main/java/com/matt/flashcards/Settings.java
+++ mobile/src/main/java/com/matt/flashcards/Settings.java
@@ -124,31 +124,47 @@ public final class Settings {
     }
 
     public static void loadData(Context context) {
+        // Make sure the data is only read in once from the file
         if (dataLoaded) return;
         dataLoaded = true;
 
         try {
+            // Get ready to read in the JSON data
             BufferedReader inputStream = new BufferedReader(
                     new InputStreamReader(context.openFileInput(FILE_NAME), "UTF-8"));
 
+            // Since all the JSON data is on 1 line, we only need to read in 1 line from the file
             JSONObject JSONSettings = new JSONObject(inputStream.readLine());
-            JSONArray JSONAllDecks = JSONSettings.getJSONArray(DECK_KEY);
             inputStream.close();
 
+            // Get the array of decks from the settings
+            JSONArray JSONAllDecks = JSONSettings.getJSONArray(DECK_KEY);
+
             for (int i = 0; i < JSONAllDecks.length(); i++) {
+                // Get a deck from the array of decks
                 JSONObject JSONDeck = (JSONObject) JSONAllDecks.get(i);
+
+                // Create a new deck object using the title read in
                 Deck deck = new Deck(JSONDeck.getString(DECK_TITLE_KEY));
+
+                // Get an array of flashcards from the deck
                 JSONArray JSONFlashcards = JSONDeck.getJSONArray(DECK_FLASHCARDS_KEY);
+
                 for (int j = 0; j < JSONFlashcards.length(); j++) {
-                    JSONObject Flashcard = JSONFlashcards.getJSONObject(j);
+                    // Get a flashcard from the array of flashcards
+                    JSONObject JSONFlashcard = JSONFlashcards.getJSONObject(j);
+
+                    // Create a new Flashcard object and add it to the deck object
                     deck.add(new Flashcard(
-                            Flashcard.getString(SIDE_A_KEY),
-                            Flashcard.getString(SIDE_B_KEY))
+                            JSONFlashcard.getString(SIDE_A_KEY),
+                            JSONFlashcard.getString(SIDE_B_KEY))
                     );
                 }
+                // Add the new deck to the theDeckOfDecks
                 theDeckOfDecks.add(deck);
             }
         } catch (FileNotFoundException e) {
+            // It's likely the file was not found because the program ran for the first time
         } catch (JSONException | IOException e) {
             new AlertDialog.Builder(context)
                     .setTitle("Error")
@@ -163,22 +179,30 @@ public final class Settings {
         JSONArray JSONAllDecks = new JSONArray();
 
         try {
-            FileOutputStream outputStream = context.openFileOutput(FILE_NAME, Context.MODE_PRIVATE);
             for (Deck deck : theDeckOfDecks) {
+                // Create a new JSON deck to hold all the flashcards
                 JSONArray JSONDeck = new JSONArray();
+
                 for (Flashcard flashcard : deck) {
+                    // Adds a new JSON flashcard to the deck
                     JSONDeck.put(new JSONObject()
                             .put(SIDE_A_KEY, flashcard.getSideA())
                             .put(SIDE_B_KEY, flashcard.getSideB())
                     );
                 }
+                // Adds the JSON deck to the array of decks
                 JSONAllDecks.put(new JSONObject()
                         .put(DECK_TITLE_KEY, deck.getTitle())
                         .put(DECK_FLASHCARDS_KEY, JSONDeck)
                 );
             }
+
+            // Adds the array of decks to the settings
             JSONSettings.put(DECK_KEY, JSONAllDecks);
-            outputStream.write(JSONSettings.toString().getBytes());
+
+            // Save the JSON data to the file
+            FileOutputStream outputStream = context.openFileOutput(FILE_NAME, Context.MODE_PRIVATE);
+            outputStream.write(JSONSettings.toString().getBytes()); // Convert to binary and write
             outputStream.close();
 
         } catch (JSONException | IOException e) {
