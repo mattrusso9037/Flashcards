31d457e3c95f6bb8ac430b55b217e5d890f55ae5
David Pirraglia <dpirraglia2@student.sjcny.edu>
2018-03-31 03:06:30 -0400

    Created a new DragItemAdapter

---
+++ mobile/src/main/java/com/matt/flashcards/FlashcardDragItemAdapter.java
@@ -0,0 +1,52 @@
+package com.matt.flashcards;
+
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.mylibrary.Flashcard;
+import com.woxthebox.draglistview.DragItemAdapter;
+
+import java.util.List;
+
+public class FlashcardDragItemAdapter extends DragItemAdapter<Flashcard, FlashcardDragItemAdapter.ViewHolder> {
+
+    private List<Flashcard> flashcardList;
+
+    public FlashcardDragItemAdapter(List<Flashcard> flashcardList) {
+        this.flashcardList = flashcardList;
+        setItemList(flashcardList);
+    }
+
+    @Override
+    public long getUniqueItemId(int position) {
+        return 0;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ViewHolder(
+                LayoutInflater.from(parent.getContext()).inflate(R.layout.flashcard_item, parent, false),
+                R.id.flashcard_item,
+                true);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        super.onBindViewHolder(holder, position);
+        holder.flashcardTextView.setText(flashcardList.get(position).getSideA());
+    }
+
+    public class ViewHolder extends DragItemAdapter.ViewHolder {
+
+        public TextView flashcardTextView;
+
+        public ViewHolder(View itemView, int handleResId, boolean dragOnLongPress) {
+            super(itemView, handleResId, dragOnLongPress);
+            flashcardTextView = itemView.findViewById(R.id.flashcard_item_text);
+        }
+    }
+}
