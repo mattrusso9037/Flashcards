7d97e0c8229e83943fe3f14394c52f64008230f8
David Pirraglia <dpirraglia2@student.sjcny.edu>
2018-03-31 02:47:19 -0400

    Created a new activity for the FlashcardDragListViewActivity

---
+++ mobile/src/main/java/com/matt/flashcards/FlashcardDragListViewActivity.java
@@ -0,0 +1,13 @@
+package com.matt.flashcards;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FlashcardDragListViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flashcard_drag_list_view);
+    }
+}
