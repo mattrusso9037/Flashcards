dd0a1c666cd694d0ec6a69c13bd41e6eb4631685
David Pirraglia <dpirraglia2@student.sjcny.edu>
2018-04-01 19:27:06 -0400

    Added Edit and Delete buttons to the Flashcard DragListView

--- mobile/src/main/java/com/matt/flashcards/FlashcardDragItemAdapter.java
+++ mobile/src/main/java/com/matt/flashcards/FlashcardDragItemAdapter.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.mylibrary.Flashcard;
@@ -19,10 +21,18 @@ public class FlashcardDragItemAdapter extends DragItemAdapter<Flashcard, Flashca
 
     private Context context;
     private List<Flashcard> flashcardList;
+    private boolean showEditDeleteButtons;
 
     public FlashcardDragItemAdapter(Context context, List<Flashcard> flashcardList) {
         this.context = context;
         this.flashcardList = flashcardList;
+        setItemList(flashcardList);
+    }
+
+    public FlashcardDragItemAdapter(Context context, List<Flashcard> flashcardList, boolean showEditDeleteButtons) {
+        this.context = context;
+        this.flashcardList = flashcardList;
+        this.showEditDeleteButtons = showEditDeleteButtons;
         setItemList(flashcardList);
     }
 
@@ -41,18 +51,43 @@ public class FlashcardDragItemAdapter extends DragItemAdapter<Flashcard, Flashca
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
         super.onBindViewHolder(holder, position);
         holder.flashcardTextView.setText(flashcardList.get(position).getSideA());
+
+        if (showEditDeleteButtons) {
+
+            holder.editDeleteLayout.setVisibility(View.VISIBLE);
+
+            holder.editView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    new DebugToast(context, "Edit Clicked: " + position);
+                }
+            });
+
+            holder.deleteView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    new DebugToast(context, "Delete Clicked: " + position);
+                }
+            });
+        }
     }
 
     public class ViewHolder extends DragItemAdapter.ViewHolder {
 
         public TextView flashcardTextView;
+        public ImageView editView;
+        public ImageView deleteView;
+        public LinearLayout editDeleteLayout;
 
         public ViewHolder(View itemView, int handleResId, boolean dragOnLongPress) {
             super(itemView, handleResId, dragOnLongPress);
             flashcardTextView = itemView.findViewById(R.id.flashcard_item_text);
+            editView = itemView.findViewById(R.id.flashcard_item_edit);
+            deleteView = itemView.findViewById(R.id.flashcard_item_delete);
+            editDeleteLayout = itemView.findViewById(R.id.flashcard_item_actions);
         }
 
         @Override
