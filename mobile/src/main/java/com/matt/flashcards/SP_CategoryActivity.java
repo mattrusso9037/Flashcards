cd528002cd18ab3d7649bc697c9d873cd4a57e6a
David Pirraglia <dpirraglia2@student.sjcny.edu>
2018-04-14 20:08:37 -0400

    The shuffle button will now if there's any flashcards instead

--- mobile/src/main/java/com/matt/flashcards/SP_CategoryActivity.java
+++ mobile/src/main/java/com/matt/flashcards/SP_CategoryActivity.java
@@ -246,56 +246,53 @@ public class SP_CategoryActivity extends AppCompatActivity implements GoogleApiC
     }
 
     private void shuffleAction() {
-        switch (Settings.theDeckOfDecks.size()) {
-            case 0:
-                new AlertDialog.Builder(SP_CategoryActivity.this)
-                        .setTitle(R.string.error)
-                        .setMessage(R.string.error_no_decks)
-                        .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                dialog.dismiss();
+        if (!Settings.areThereFlashcards()) {
+            new AlertDialog.Builder(SP_CategoryActivity.this)
+                    .setTitle(R.string.error)
+                    .setMessage(R.string.shuffle_error_no_flashcards)
+                    .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    })
+                    .show();
+        } else if (Settings.theDeckOfDecks.size() == 1) {
+            Settings.generateShuffledDeck(new boolean[]{true});
+            startActivity(new Intent(SP_CategoryActivity.this, FlashcardActivity.class)
+                    .putExtra("shuffleMode", true));
+        } else {
+            final boolean[] checkedItems = new boolean[Settings.theDeckOfDecks.size()];
+            new AlertDialog.Builder(SP_CategoryActivity.this)
+                    .setTitle(R.string.shuffle_mode)
+                    // checkedItems won't update properly without this
+                    .setMultiChoiceItems(Settings.getAllDeckTitles(), checkedItems,
+                            new DialogInterface.OnMultiChoiceClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which, boolean isChecked) {}
+                            })
+                    .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            Settings.generateShuffledDeck(checkedItems);
+                            startActivity(new Intent(SP_CategoryActivity.this, FlashcardActivity.class)
+                                    .putExtra("shuffleMode", true));
+                        }
+                    })
+                    .setNeutralButton(R.string.select_all, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            for (int i = 0; i < checkedItems.length; i++) {
+                                checkedItems[i] = true;
                             }
-                        })
-                        .show();
-                break;
-            case 1:
-                Settings.generateShuffledDeck(new boolean[]{true});
-                startActivity(new Intent(SP_CategoryActivity.this, FlashcardActivity.class)
-                                .putExtra("shuffleMode", true));
-                break;
-            default:
-                final boolean[] checkedItems = new boolean[Settings.theDeckOfDecks.size()];
-                new AlertDialog.Builder(SP_CategoryActivity.this)
-                        .setTitle(R.string.shuffle_mode)
-                        // checkedItems won't update properly without this
-                        .setMultiChoiceItems(Settings.getAllDeckTitles(), checkedItems,
-                                new DialogInterface.OnMultiChoiceClickListener() {
-                                    @Override
-                                    public void onClick(DialogInterface dialog, int which, boolean isChecked) {}
-                                })
-                        .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                Settings.generateShuffledDeck(checkedItems);
-                                startActivity(new Intent(SP_CategoryActivity.this, FlashcardActivity.class)
-                                        .putExtra("shuffleMode", true));
-                            }
-                        })
-                        .setNeutralButton(R.string.select_all, new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                for (int i = 0; i < checkedItems.length; i++) {
-                                    checkedItems[i] = true;
-                                }
-                                Settings.generateShuffledDeck(checkedItems);
-                                startActivity(
-                                        new Intent(SP_CategoryActivity.this, FlashcardActivity.class)
-                                                .putExtra("shuffleMode", true));
-                            }
-                        })
-                        .setNegativeButton(R.string.cancel, null)
-                        .show();
+                            Settings.generateShuffledDeck(checkedItems);
+                            startActivity(
+                                    new Intent(SP_CategoryActivity.this, FlashcardActivity.class)
+                                            .putExtra("shuffleMode", true));
+                        }
+                    })
+                    .setNegativeButton(R.string.cancel, null)
+                    .show();
         }
     }
 
